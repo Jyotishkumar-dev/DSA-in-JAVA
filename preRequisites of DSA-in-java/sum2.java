fefaa0885a94b4eb4e8b9fed9e93ccdd7a37d750
jyotish kumar <jyotishkumarjhar@gmail.com>
2026-07-31 23:32:53 +0530

    java

---
+++ preRequisites of DSA-in-java/sum2.java
@@ -0,0 +1,11 @@
+public class sum2{
+    public static void main(String[] args) {
+        int a=10;
+        int b=30;
+        int sum=a+b;
+
+        System.out.println("Sum  = " + sum);
+        
+    }
+    
+}
